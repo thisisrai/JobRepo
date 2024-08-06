c3a72dc3449763d772289afce09d02210c1ec507
thisisrai <thisisrailee@gmail.com>
2024-08-05 20:22:05 -0700

    make username unique

---
+++ db/migrate/20240806031602_add_index_to_users_username.rb
@@ -0,0 +1,4 @@
+class AddIndexToUsersUsername < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
-require "test_helper"
+require 'rails_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe User, type: :model do
+  before do
+    @user = User.new(username: "testuser", password: "password", password_confirmation: "password", age: 25)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@user).to be_valid
+  end
+
+  it "is not valid without a username" do
+    @user.username = nil
+    expect(@user).to_not be_valid
+  end
+
+  it "is not valid with a duplicate username" do
+    duplicate_user = @user.dup
+    @user.save
+    expect(duplicate_user).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    @user.password = nil
+    expect(@user).to_not be_valid
+  end
+
+  it "is not valid without a matching password confirmation" do
+    @user.password_confirmation = "differentpassword"
+    expect(@user).to_not be_valid
+  end
+
+  it "destroys associated jobs when the user is destroyed" do
+    @user.save
+    @user.jobs.create!(title: "Developer", application_url: "job.com", company: "Tech Co")
+    expect { @user.destroy }.to change { Job.count }.by(-1)
+  end
 end
