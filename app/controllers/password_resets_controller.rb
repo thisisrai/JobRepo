f189d5bea2a090570aecbfc82e876124b2aa51fc
thisisrai <thisisrailee@gmail.com>
2024-09-04 16:02:36 -0700

    allow user to access password recovery

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,5 @@
 class PasswordResetsController < ApplicationController
+  skip_before_action :authorized
   # POST /password_resets
   def create
     user = User.find_by(username: params[:username])
