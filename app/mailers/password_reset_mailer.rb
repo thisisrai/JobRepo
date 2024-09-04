0e15f94f834dbb04cd2d5ec023c5aa71b2beab83
thisisrai <thisisrailee@gmail.com>
2024-09-04 16:17:25 -0700

    attempt to fix password reset

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,7 +1,10 @@
 class PasswordResetMailer < ApplicationMailer
-  def reset_email
-    @user = params[:user]
-    @reset_url = password_reset_url(token: @user.reset_password_token)
+  default from: 'contact@coffeejob.io'
+
+
+  def reset_email(user)
+    @user = user
+    @token = @user.reset_password_token
 
     mail(to: @user.username, subject: 'Password Reset Instructions')
   end
