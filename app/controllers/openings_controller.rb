9892c2579b4b3fabbadd22afe1bfe7a8f02fe964
thisisrai <thisisrailee@gmail.com>
2024-10-29 18:04:27 -0700

    update opening ordering

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -4,7 +4,7 @@ class OpeningsController < ApplicationController
     per_page = params[:per_page] || 50
 
     # Start with filtering based on location and title (case-insensitive)
-    openings = Opening.order(updated_at: :desc)
+    openings = Opening.order(posted_on: :desc)
     openings = openings.where('location ILIKE ?', "%#{params[:location]}%") if params[:location].present?
     openings = openings.where('title ILIKE ?', "%#{params[:title]}%") if params[:title].present?
 
