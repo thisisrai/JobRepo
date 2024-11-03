51ec6a82a1903ce664ccdd139ea046f9dd3b6d27
thisisrai <thisisrailee@gmail.com>
2024-11-03 07:20:43 -0800

    add total for pagination

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -8,10 +8,17 @@ class OpeningsController < ApplicationController
     openings = openings.where('location ILIKE ?', "%#{params[:location]}%") if params[:location].present?
     openings = openings.where('title ILIKE ?', "%#{params[:title]}%") if params[:title].present?
 
+    # Get total count before pagination
+    total_count = openings.count
+
     # Apply pagination after filtering
     paginated_openings = openings.page(page).per(per_page)
 
-    render json: paginated_openings
+    # Render JSON with paginated data and total count
+    render json: {
+      total_count: total_count,
+      data: paginated_openings
+    }
   end
 
   def authorized
